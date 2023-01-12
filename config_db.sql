935e3add72449cd236bae4f7959cdd60c9450e18
Dallas Plunkett <dallasplunkett@gmail.com>
2023-01-11 16:19:03 -0800

    cleaned up the sql file for creating the db

--- config_db.sql
+++ config_db.sql
@@ -10,21 +10,3 @@ create table tracks (
 	latitude float not null,
 	longitude float not null
 );
-
--- insert into tracks
--- values (
--- 	'2023-01-09',
--- 	'20:50:00',
--- 	'sun',
--- 	22.05,
--- 	130.7167
--- );
-
--- insert into tracks
--- values (
--- 	'2023-01-09',
--- 	'20:50:00',
--- 	'moon',
--- 	19.2167,
--- 	83.2833
--- );
